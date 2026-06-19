b52fae55994ee9277187ed21f77108bf54a4ab71
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2026-06-19 16:11:07 +0530

    Implement power function to calculate exponentiation

---
+++ Power.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int power(int base, int x){
+    if(x==0){
+        return 1;
+    }
+    return(base*power(base,x-1));
+}
+
+int main(){
+    int base = 5 , x = 3;
+    cout<<"Required power is "<<power(base,x);
+}
